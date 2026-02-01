049cd955f678d1701a13a99184315a45246c0687
mizdra <pp.mizdra@gmail.com>
2026-02-01 22:12:21 +0900

    format output

--- volar.ts
+++ volar.ts
@@ -11,21 +11,21 @@ if (!sourceFile) throw new Error("Source file not found: " + targetFile);
 
 const position = getPosition(sourceFile, 2, 8); // Position of 'a_1' in styles.a_1
 
-console.log(`=== Definitions for a_1 ===`);
+console.log(`=== getDefinitionAtPosition for a_1 ===`);
 const definitions = languageService.getDefinitionAtPosition(
   targetFile,
   position,
 );
 console.dir(definitions?.map(extractLocation), { depth: null });
 
-console.log(`\n=== References for a_1 ===`);
+console.log(`\n=== findReferences for a_1 ===`);
 const references = languageService.findReferences(targetFile, position);
 console.dir(references?.map(reference => ({
   definition: extractLocation(reference.definition),
   references: reference.references.map(ref => extractLocation(ref)),
 })), { depth: null });
 
-console.log(`\n=== RenameLocations for a_1 ===`);
+console.log(`\n=== findRenameLocations for a_1 ===`);
 const renameLocations = languageService.findRenameLocations(
   targetFile,
   position,
